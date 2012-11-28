d47789d72bf8009005ab3c549f73a9091a18c51e
lariverosc <lariverosc@gmail.com>
2012-11-28 10:50:47 -0500

    Multiple improvements
    Remove Thread Spring bean
    Add SpringWorkerPool
    Add init and destroy methods to manage threads within SpringWorker and SpringWorkerPool

--- src/main/java/net/lariverosc/jesquespring/SpringWorker.java
+++ src/main/java/net/lariverosc/jesquespring/SpringWorker.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Alejandro Riveros Cruz <lariverosc@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.lariverosc.jesquespring;
 
 import java.util.Collection;
@@ -68,5 +84,21 @@ public class SpringWorker extends WorkerImpl implements ApplicationContextAware 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
+	}
+	
+	/**
+	 * Convenient initialization method for the Spring container
+	 */
+	public void init(){
+		logger.info("Start a new thread for SpringWorker");
+		new Thread(this).start();
+	}
+	
+	/**
+	 * Convenient destroy method for the Spring container
+	 */
+	public void destroy(){
+		logger.info("End the SpringWorker thread");
+		end(true);
 	}
 }
